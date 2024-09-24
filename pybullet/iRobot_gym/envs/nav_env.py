c8ed01d2552906b2f7879d3882415274a16fce5f
dean-south <solomon.mdean@gmail.com>
2024-09-24 22:57:44 +0200

    add model and minor reward change

--- pybullet/iRobot_gym/envs/nav_env.py
+++ pybullet/iRobot_gym/envs/nav_env.py
@@ -361,7 +361,7 @@ class RewardCarryOn:
             reward = -200
         
         elif abs(phi) < 1 and v_x > 0:
-            reward *= max(abs(phi), 0.05)
+            reward *= max(abs(phi), 0.5)
             
         return reward
 
